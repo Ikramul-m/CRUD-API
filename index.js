622f0aa2574c6aabac7ee2cf2bffcec923776f5a
Ikramul-m <i20727172@gmail.com>
2024-05-20 22:50:21 +0530

    Connect Database

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const app = express();
 const PORT = 3000;
 const hostName = "localhost";
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://${hostName}:${PORT}`);
-});
-
 app.get("/", (req, res) => {
   res.send("Hello from API Server...");
 });
+
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(PORT, () => {
+      console.log(`Server listening on http://${hostName}:${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
